33fef5ee2dc29a2e187897bf86789869bb3812c2
Ingo Griebsch <ingo.griebsch@gbtec.de>
2018-01-16 09:24:08 +0100

    doc(project): add a more detailed explanation of the behavior

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-// FIXME As long as we don't use this annotation the actuator endpoints are protected based on the configured user/password. If we
-// use it, we are getting an 'Full authentication is required to access this resource'.
+// FIXME As long as we don't use this annotation the actuator endpoints are protected based on the configured user/password.
+// If we use the annotation, we are getting a 'Full authentication is required to access this resource' if calling a sensitive
+// actuator endpoint. And all not sensitive endpoints are only returning minimal information.
 @EnableResourceServer
 @SpringBootApplication
 public class DemoApplication {

91b7e6e89974de7cd4db2094b8f626f8e17f32c0
Ingo Griebsch <ingo.griebsch@gbtec.de>
2018-01-16 09:22:08 +0100

    refactor(project): use @EnableResourceServer to show the problem

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 // FIXME As long as we don't use this annotation the actuator endpoints are protected based on the configured user/password. If we
 // use it, we are getting an 'Full authentication is required to access this resource'.
-// @EnableResourceServer
+@EnableResourceServer
 @SpringBootApplication
 public class DemoApplication {
 
